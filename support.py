0d62ad16bf86a3f4a8619f0703b0727c9f0d5db5
Felix Musau <fellyMfelix@gmail.com>
2025-11-06 19:54:19 +0300

    arranging notebook

--- support.py
+++
@@ -1,8 +0,0 @@
-### adding contact support page
-import streamlit as st
-st.title("🛠️ Contact Support")
-st.write("If you have any questions or need assistance, please reach out to us!")
-st.write("Email: support@example.com")
-st.write("Phone: +1-800-123-4567")
-st.write("We're here to help you 24/7!")
-
